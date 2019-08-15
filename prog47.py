e4d40c2c5b44f78b1816387d12695a5fa6a563a7
Poornimamunuswamy <34741561+Poornimamunuswamy@users.noreply.github.com>
2019-08-15 17:09:47 +0530

    Add files via upload

---
+++ prog47.py
@@ -0,0 +1,7 @@
+a=int(input(""))
+arr=[]
+for i in range(a):
+    n=int(input(""))
+    arr.append(n)
+print(min(arr), max(arr))
+    
---
+++ prog48.py
@@ -0,0 +1,6 @@
+a=int(input(""))
+arr=[]
+for i in range(a):
+    n=int(input(""))
+    arr.append(n)
+print(sum(arr)//len(arr))
